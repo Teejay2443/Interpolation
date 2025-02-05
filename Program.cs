05d585d4bb53247112867201ea827d591d1334b4
Teejay2443 <tijaniabdulafeez05@gmail.com>
2025-02-05 08:56:11 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+//  string myName = " Tijani AbdulAfeez,";
+// int myAge = 17;
+// double myHeight = 4.5;
+// string myState = "Ogun state";
+// Console.WriteLine($" Myname is {myName},\n I am {myAge} years old,\n My height is {myHeight},\n I am from {myState}");
+
+//if i want to write is Without line break
+ string myName = " Tijani AbdulAfeez,";
+ int myAge = 17;
+ double myHeight = 4.5;
+ string myState = "Ogun state";
+Console.WriteLine($" Myname is {myName}, I am {myAge} years old, My height is {myHeight}, I am from {myState}");
+
